38b06ad2fea94da41c0a0c86e41f1f09203a3cd5
tooltip <192393975+bsky-labs@users.noreply.github.com>
2025-03-02 15:19:10 -0800

    feat: update core html and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Playground from './components/ui/oauth-playground'
+import OAuthClientProvider from './contexts/oauth'
 
 function App() {
-
   return (
-    <></>
+    <>
+      <OAuthClientProvider>
+        <Playground className="" />
+      </OAuthClientProvider>
+    </>
   )
 }
 
