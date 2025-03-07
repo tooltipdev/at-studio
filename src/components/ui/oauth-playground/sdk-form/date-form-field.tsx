36280442bac1e2cc07a237f84818fcf0c2b564c5
tooltip <192393975+tooltipdev@users.noreply.github.com>
2025-03-07 11:49:13 -0800

    feat(date-form-field): format file

--- src/components/ui/oauth-playground/sdk-form/date-form-field.tsx
+++ src/components/ui/oauth-playground/sdk-form/date-form-field.tsx
@@ -1,75 +1,74 @@
 import {
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from '../../shadcn/form';
-  
-  import { Input } from '../../shadcn/input';
-  import { UseFormReturn } from 'react-hook-form';
-  import { Badge } from '../../shadcn/badge';
-  
-  function DateFormField({
-    schema,
-    form,
-    path,
-  }: {
-    schema: { [key: string]: any };
-    form: UseFormReturn;
-    path: string;
-  }) {
-    let defaultValue =
-      schema._def?.defaultValue || schema._def?.innerType?._def?.defaultValue || schema.defaultValue;
-  
-    let isOptional = schema.isOptional;
-  
-    if (typeof isOptional === 'function') isOptional = isOptional();
-  
-    const schemaMetadata = schema.meta ? schema.meta() : {};
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../../shadcn/form';
 
-    return (
-      <FormField
-        control={form.control}
-        key={path}
-        name={path}
-        render={({ field }) => {
-          return (
-            <FormItem>
-              <FormLabel>
-                {schemaMetadata.label || path}
-                {isOptional && (
-                  <>
-                    &nbsp;&nbsp;
-                    <Badge className="opacity-80" variant="secondary">
-                      Optional
-                    </Badge>
-                  </>
-                )}
-              </FormLabel>
-              <FormDescription>{schema.description}</FormDescription>
-              <FormControl>
-                <Input
-                  id="kitty"
-                  type="datetime-local"
-                  defaultValue={defaultValue ? defaultValue() : undefined}
-                  className="bg-white"
-                  key={field.name}
-                  placeholder=""
-                  onChange={(e) => {
-                    const date = new Date(e.target.value);
-                    form.setValue(field.name, date.toISOString());
-                  }}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          );
-        }}
-      />
-    );
-  }
-  
-  export default DateFormField;
-  
+import { Input } from '../../shadcn/input';
+import { UseFormReturn } from 'react-hook-form';
+import { Badge } from '../../shadcn/badge';
+
+function DateFormField({
+  schema,
+  form,
+  path,
+}: {
+  schema: { [key: string]: any };
+  form: UseFormReturn;
+  path: string;
+}) {
+  let defaultValue =
+    schema._def?.defaultValue || schema._def?.innerType?._def?.defaultValue || schema.defaultValue;
+
+  let isOptional = schema.isOptional;
+
+  if (typeof isOptional === 'function') isOptional = isOptional();
+
+  const schemaMetadata = schema.meta ? schema.meta() : {};
+
+  return (
+    <FormField
+      control={form.control}
+      key={path}
+      name={path}
+      render={({ field }) => {
+        return (
+          <FormItem>
+            <FormLabel>
+              {schemaMetadata.label || path}
+              {isOptional && (
+                <>
+                  &nbsp;&nbsp;
+                  <Badge className="opacity-80" variant="secondary">
+                    Optional
+                  </Badge>
+                </>
+              )}
+            </FormLabel>
+            <FormDescription>{schema.description}</FormDescription>
+            <FormControl>
+              <Input
+                id="kitty"
+                type="datetime-local"
+                defaultValue={defaultValue ? defaultValue() : undefined}
+                className="bg-white"
+                key={field.name}
+                placeholder=""
+                onChange={(e) => {
+                  const date = new Date(e.target.value);
+                  form.setValue(field.name, date.toISOString());
+                }}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />
+  );
+}
+
+export default DateFormField;
