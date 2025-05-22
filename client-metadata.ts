622ffd20411d4752da1ab4a086d76fdb905486cc
tooltip <192393975+tooltipdev@users.noreply.github.com>
2025-05-22 09:31:42 -0700

    feat(client-metadata.ts): utilize BASE_PATH in client_id value

--- client-metadata.ts
+++ client-metadata.ts
@@ -1,9 +1,9 @@
-const { HOST, CLIENT_METADATA_CLIENT_NAME } = process.env;
+const { HOST, BASE_PATH, CLIENT_METADATA_CLIENT_NAME } = process.env;
 const host = HOST!.replace(/\/+$/, '');
 
 export default {
   client_name: CLIENT_METADATA_CLIENT_NAME,
-  client_id: `${host}/client-metadata.json`,
+  client_id: `${host}/${BASE_PATH}/client-metadata.json`,
   client_uri: host,
   logo_uri: 'https://avatars.githubusercontent.com/u/192393975',
   redirect_uris: [host],
