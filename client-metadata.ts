974e629b27bbb921fd52c57d36672e4efc09cd46
tooltip <192393975+tooltipdev@users.noreply.github.com>
2025-05-22 09:08:44 -0700

    feat(client-metadata.ts): strip trailing slashes from HOST

--- client-metadata.ts
+++ client-metadata.ts
@@ -1,11 +1,12 @@
 const { HOST, CLIENT_METADATA_CLIENT_NAME } = process.env;
+const host = HOST!.replace(/\/+$/, '');
 
 export default {
   client_name: CLIENT_METADATA_CLIENT_NAME,
-  client_id: `${HOST}client-metadata.json`,
-  client_uri: HOST,
+  client_id: `${host}/client-metadata.json`,
+  client_uri: host,
   logo_uri: 'https://avatars.githubusercontent.com/u/192393975',
-  redirect_uris: [HOST],
+  redirect_uris: [host],
   scope: 'atproto transition:generic',
   grant_types: ['authorization_code', 'refresh_token'],
   response_types: ['code'],
