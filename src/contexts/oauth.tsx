f8be1a90f4702ac894a07f5bb756467617df3d1c
tooltip <192393975+tooltipdev@users.noreply.github.com>
2025-05-23 02:07:10 -0700

    feat: separate oauth context provider from oauth context declaration

--- src/contexts/oauth.tsx
+++ src/contexts/oauth.tsx
@@ -1,40 +1,14 @@
 import OAuthClient from '@/services/OAuthClient';
-import React, { createContext, useEffect, useState } from 'react';
+import { createContext } from 'react';
 
 export type OAuthClientMetadata = {
   client: OAuthClient | null;
   isLoading: boolean;
   isLoadingError: boolean;
-}
-
-export const OAuthClientContext = createContext<OAuthClientMetadata>({client: null, isLoading: true, isLoadingError: false});
-export default function OAuthClientProvider({ children }: { children: React.ReactNode }) {
-  const [client, setClient] = useState<null | OAuthClient>(null)
-  const [initFailed, setInitFailed] = useState(false)
-  const initClient = async () => {
-    try {
-      const newClient = new OAuthClient()
-
-      await newClient.init()
-
-      newClient.events.on('SIGN_OUT', () => {
-        initClient()
-      })
-
-      setClient(newClient)
-    } catch (err) {
-      console.log(err)
-      setInitFailed(true)
-    }
-  }
-
-  useEffect(() => {
-    initClient();
-  }, [])
-
-  return (
-    <OAuthClientContext.Provider value={{client, isLoading: client ? false : true, isLoadingError: initFailed}}>
-      {children}
-    </OAuthClientContext.Provider>
-  );
 };
+
+export const OAuthClientContext = createContext<OAuthClientMetadata>({
+  client: null,
+  isLoading: true,
+  isLoadingError: false,
+});
