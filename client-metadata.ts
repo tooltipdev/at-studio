3d247c51c284f21a9d8b363e396b1719883b2103
tooltip <192393975+tooltipdev@users.noreply.github.com>
2025-05-23 12:29:43 -0700

    feat(client-metadata.ts): decouple oauth client uris from dev server network configuration

--- client-metadata.ts
+++ client-metadata.ts
@@ -1,24 +1,25 @@
 const {
-  HOST,
-  BASE_PATH,
   OAUTH_CLIENT_METADATA_CLIENT_NAME,
   OAUTH_CLIENT_METADATA_AVATAR_URI,
   OAUTH_CLIENT_METADATA_GRANT_TYPES_CSV,
   OAUTH_CLIENT_METADATA_SCOPES,
+  OAUTH_CLIENT_METADATA_REDIRECT_URIS_CSV,
+  OAUTH_CLIENT_METADATA_CLIENT_URI,
 } = process.env;
 
-const host = HOST!.replace(/\/+$/, '');
-const uri = `${host}${BASE_PATH}/`;
 const grantTypes = OAUTH_CLIENT_METADATA_GRANT_TYPES_CSV
   ? OAUTH_CLIENT_METADATA_GRANT_TYPES_CSV.split(',').map((type) => type.trim())
   : ['authorization_code', 'refresh_token'];
+const redirectUris = OAUTH_CLIENT_METADATA_REDIRECT_URIS_CSV
+  ? OAUTH_CLIENT_METADATA_REDIRECT_URIS_CSV.split(',').map((uri) => uri.trim())
+  : [OAUTH_CLIENT_METADATA_CLIENT_URI];
 
 export default {
   client_name: OAUTH_CLIENT_METADATA_CLIENT_NAME || 'bsky-oauth-playground',
-  client_id: `${uri}client-metadata.json`,
-  client_uri: uri,
+  client_id: `${OAUTH_CLIENT_METADATA_CLIENT_URI}client-metadata.json`,
+  client_uri: OAUTH_CLIENT_METADATA_CLIENT_URI,
   logo_uri: OAUTH_CLIENT_METADATA_AVATAR_URI || '',
-  redirect_uris: [uri],
+  redirect_uris: [redirectUris],
   scope: OAUTH_CLIENT_METADATA_SCOPES || 'atproto transition:generic',
   grant_types: grantTypes,
   response_types: ['code'],
