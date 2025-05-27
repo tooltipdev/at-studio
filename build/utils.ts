752550019de66037d0c2ba37f6022bad838a7df5
tooltip <192393975+tooltipdev@users.noreply.github.com>
2025-05-27 13:59:23 -0700

    feat(build): add build directory with utils file

---
+++ build/utils.ts
@@ -0,0 +1,11 @@
+export function ensureSingleLeadingSlash(input: string): string {
+  return `/${input.replace(/^\/+/, '')}`;
+}
+
+export function removeTrailingSlashes(input: string): string {
+  return input.replace(/\/+$/, '');
+}
+
+export function ensureSingleTrailingSlash(input: string): string {
+  return `${input.replace(/\/+$/, '')}/`;
+}
