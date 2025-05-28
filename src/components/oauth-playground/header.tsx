37f63e883e0f6c70d5e0115538166b332b1f6f25
tooltip <192393975+tooltipdev@users.noreply.github.com>
2025-05-28 16:06:29 -0700

    feat: refactor playground header

--- src/components/oauth-playground/header.tsx
+++ src/components/oauth-playground/header.tsx
@@ -20,6 +20,80 @@ import { Badge } from '../shadcn/badge';
 import { Agent } from '@atproto/api';
 import OAuthClient from '@/services/OAuthClient';
 
+function AboutDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button variant="ghost">About</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>About this app</DialogTitle>
+          <br />
+          <br />
+          <DialogDescription>
+            This web app lets you easily experiment with the{' '}
+            <a className="underline" href="https://atproto.com/">
+              @ATProtocol
+            </a>{' '}
+            via the{' '}
+            <a
+              className="underline"
+              href="https://www.npmjs.com/package/@atproto/oauth-client-browser"
+            >
+              browser client SDK implementation
+            </a>
+            .
+            <br />
+            <br />
+            Currently, only{' '}
+            <a className="underline" href="https://bsky.social">
+              bsky.social
+            </a>{' '}
+            instances are supported, but other PDS's and entryways will be supported in the future.
+            <br />
+            <br />
+            Sign in with you{' '}
+            <a className="underline" href="https://bsky.app/">
+              bsky.app
+            </a>{' '}
+            account to interact with the protocol.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function ProfileDropdown({
+  profile,
+  client,
+}: {
+  profile: Awaited<ReturnType<typeof Agent.prototype.getProfile>>;
+  client: OAuthClient;
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage src={profile.data.avatar} /> <AvatarFallback>...</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>@{profile.data.handle}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            client.signOut();
+          }}
+        >
+          Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
 function Header({
   profile,
   client,
@@ -32,48 +106,7 @@ function Header({
   return (
     <>
       <div className="h-auto p-2 w-screen max-w-[960px] flex items-center">
-        <Dialog>
-          <DialogTrigger>
-            <Button variant="ghost">About</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>About this app</DialogTitle>
-              <br />
-              <br />
-              <DialogDescription>
-                This web app lets you easily experiment with the{' '}
-                <a className="underline" href="https://atproto.com/">
-                  @ATProtocol
-                </a>{' '}
-                via the{' '}
-                <a
-                  className="underline"
-                  href="https://www.npmjs.com/package/@atproto/oauth-client-browser"
-                >
-                  browser client SDK implementation
-                </a>
-                .
-                <br />
-                <br />
-                Currently, only{' '}
-                <a className="underline" href="https://bsky.social">
-                  bsky.social
-                </a>{' '}
-                instances are supported, but other PDS's and entryways will be supported in the
-                future.
-                <br />
-                <br />
-                Sign in with you{' '}
-                <a className="underline" href="https://bsky.app/">
-                  bsky.app
-                </a>{' '}
-                account to interact with the protocol.
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-
+        <AboutDialog />
         <Button
           variant="ghost"
           onClick={() =>
@@ -84,28 +117,7 @@ function Header({
         </Button>
         <Badge className="ml-2">v{version}</Badge>
         <div className="flex-1"></div>
-        {profile && (
-          <div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Avatar>
-                  <AvatarImage src={profile.data.avatar} /> <AvatarFallback>...</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>@{profile.data.handle}</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  onClick={() => {
-                    client.signOut();
-                  }}
-                >
-                  Sign Out
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        )}
+        {profile && <ProfileDropdown profile={profile} client={client} />}
       </div>
     </>
   );
